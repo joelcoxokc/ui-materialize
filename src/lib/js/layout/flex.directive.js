960f5c906233536a36ee6466f379ac7bfe8ab3f9
loqi <loqi@cruzio.com>
2015-01-27 01:39:58 -0800

    refactor: delete derelicts from src/client/app/modules/stylus

--- src/lib/js/layout/flex.directive.js
+++ src/lib/js/layout/flex.directive.js
@@ -8,17 +8,16 @@
     /* @inject */
     function mzFlex() {
 
-        return  { template: '<div class="mz-flex" data-ng-transclude></div>'
-                , restrict  : 'E'
-                , scope     : {}
-                , replace   : true
-                , transclude: true
-                , controller: 'mzNavController as vm'
-                , link: link
+        return  { template   : '<div class="mz-flex" data-ng-transclude></div>'
+                , restrict   : 'E'
+                , scope      : {}
+                , replace    : true
+                , transclude : true
+                , controller : 'mzNavController as vm'
+                , link       : link
                 };
 
-        ////////////////
-
         function link(scope, element, attrs, ctrl) {}
+
       }
   }).call(this);
