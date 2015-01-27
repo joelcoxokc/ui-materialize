2c10f8ffedc7d8379fc2e2ada65274182b0f8a11
joelcoxokc <joelcox@hisimagination.com>
2015-01-26 19:51:04 -0800

    Fix Merge Conflicts

--- src/lib/js/components/button.directive.js
+++ src/lib/js/components/button.directive.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
 ;function() { 'use strict';
-=======
-;(function() { 'use strict';
-
->>>>>>> 23822a501be114f13e5405a8d0de5cc4e6054888
     angular
         .module('mz.components.button', [])
         .directive('mzBtn', mzBtn);
