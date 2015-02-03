ea7a7425e5ba17899785fcc7546c2896a6d3b687
joelcoxokc <joelcox@hisimagination.com>
2015-02-03 09:26:14 -0800

    Update: change line height on side-nav actions

--- src/client/app/core/controllers/app.controller.js
+++ src/client/app/core/controllers/app.controller.js
@@ -7,17 +7,8 @@
     /* @ngInject */
     function AppController($scope, $storage, TweenMax, Cubic) {
 
-        // console.log(mzNavApi)
-        // console.log(EventDispatcher);
-        // this.right = $Navigation.navs.right;
-        // console.log(this.right.$toggles.open())
-        // console.log($Navigation.navs.right)
-
         this.components = getComponents();
 
-        // jQuery(document).ready(function() {
-
-        // })
         function getComponents() {
             return  [ 'buttons'
                     , 'cards'
