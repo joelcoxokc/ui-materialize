4817ed02e7ad240148bf0d4f823131146418ad19
joelcoxokc <joelcox@hisimagination.com>
2015-02-02 22:16:34 -0800

    Update: Nav bar broken. Fix syntax issues

--- src/lib/js/nav/nav-side/side-collapse.js
+++ src/lib/js/nav/nav-side/side-collapse.js
@@ -1,4 +1,5 @@
 ;(function() { 'use strict';
+
     angular
         .module('mz.nav.side.collapse', [])
         .directive('sideCollapse', sideCollapse)
@@ -6,26 +7,37 @@
 
     /* @ngInject */
     function sideCollapse() {
-        return  { templateUrl : 'nav/side-collapse.html'
-                , restrict    : 'E'
-                , require     : '^sideCollapsible'
-                , scope       : { header:'@' }
-                , replace     : true
-                , transclude  : true
-                , link        : link
-                };
+
+        return { templateUrl : 'nav/side-collapse.html'
+               , restrict    : 'E'
+               , require     : '^sideCollapsible'
+               , scope       : { header:'@' }
+               , replace     : true
+               , transclude  : true
+               , link        : link
+               };
+
+        ///////////////////
+
         function link(scope, element, attrs, ctrl, transclude) {
-            var defaults;
-            var inner = element.children();
+            var inner, defaults;
+
+            inner = element.children();
 
             ///  @scope state  |  set the state for ui-sref if attribute is passed
             ///  @scope link   |  set the link for href if attribute is passed
             ///  @scope waves  |  set waves on scope, or default wave is specified. otherwise no waves used
-            scope.state ||( attrs.state  = null );
-            scope.link  ||( attrs.link   = null );
-            scope.icon  ||( attrs.icon   = null );
+            scope.state = attrs.state  || null;
+            scope.link  = attrs.link   || null;
+            scope.icon  = attrs.icon   || null;
 
             // FIXME: What's the new behavior here?
+
+            attrs.waves &&( scope.waves = 'waves-effect ' );
+            if (_.isString(attrs.waves) && attrs.waves !== 'true') {
+                scope.waves += 'waves-'+attrs.waves; }
+            else {
+                scope.waves += 'waves-light'; }
 
             //////     @transclude  |  transclude the actions
             // transclude(scope, function (clone) {
@@ -33,5 +45,4 @@
             // });
           }
       }
-
   }).call(this);
--- src/lib/js/nav/nav-side/side-collapsible.js
+++ src/lib/js/nav/nav-side/side-collapsible.js
@@ -1,4 +1,5 @@
 ;(function() { 'use strict';
+
     angular
         .module('mz.nav.side.collapsible', ['mz.nav.side.collapse'])
         .directive('sideCollapsible', sideCollapsible)
@@ -6,34 +7,45 @@
 
     /* @ngInject */
     function sideCollapsible() {
-        return  {  templateUrl: 'nav/side-collapsible.html'
-                , restrict   : 'E'
-                , scope      : true
-                , replace    : true
-                , transclude : true
-                , controller : controller
-                , link       : link
-                };
+
+        return {  templateUrl: 'nav/side-collapsible.html'
+               , restrict   : 'E'
+               , scope      : true
+               , replace    : true
+               , transclude : true
+               , controller : controller
+               , link       : link
+               };
+
         /* @ngInject */
         function controller() {
-            this.children = {};
-            this.init = function(element) { this.element = element; };
-          }
-        function link(scope, element, attrs, ctrl, transclude) {
-            var globalSides = { top:{} , right:{} , bottom:{} , left:{} };
-            var inner = element.children('ul');
-            ///  @scope   childClassList
-            ///  @scope   mobile
-            scope.childClassList = attrs.links;
-            attrs.mobile &&( scope.mobile = 'nav-mobile' );
-            ///  @class   inner
-            ///  @class   element
-            attrs.side && globalSides[attrs.side] &&( element.addClass('side-collapsible-'+attrs.side) )
-            jQuery(document).ready(function() {
-                attrs.links &&( $(element).find('.linked').addClass(attrs.links) )
-                // $('.collapsible').collapsible();
-              });
-          } // end function link
-    } // end function sideCollapsible(){...}
 
-  }).call(this);
+            this.children = {};
+
+            this.init = function(element) {
+                this.element = element;
+            }
+        }
+
+        function link(scope, element, attrs, ctrl, transclude) {
+
+                var globalSides = {  top:{} , right:{} , bottom:{} , left:{}  };
+                var inner = element.children('ul');
+
+                ///  @scope   childClassList
+                ///  @scope   mobile
+                scope.childClassList = attrs.links;
+                attrs.mobile &&( scope.mobile = 'nav-mobile' );
+
+                ///  @class   inner
+                ///  @class   element
+
+                if(attrs.side && globalSides[attrs.side]) {
+                    element.addClass('side-collapsible-'+attrs.side); }
+
+                jQuery(document).ready(function() {
+                    attrs.links && $(element).find('.linked').addClass(attrs.links);
+                    // $('.collapsible').collapsible();
+                });
+            } // end function link
+    } }).call(this);
