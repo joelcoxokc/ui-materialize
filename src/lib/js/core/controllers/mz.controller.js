d493a9d904d51999639f9ce27f942f158a0c6498
joelcoxokc <joelcox@hisimagination.com>
2015-02-04 15:27:18 -0800

    Udate

---
+++ src/lib/js/core/controllers/mz.controller.js
@@ -0,0 +1,30 @@
+;(function() { 'use strict';
+    angular
+        .module('mz.core.ctrl', [])
+        .controller('mzController', mzController)
+        ;
+
+    /* @ngAnotate */
+    function mzController($scope, $q, $RightNavigationService, $LeftNavigationService, $NavBarDelegate, mzNavApi, $rootScope) {
+        // $scope.mzNav = mzNavApi;
+        // var _this = this;
+        var sides = {
+            right : $RightNavigationService,
+            left  : $LeftNavigationService
+          };
+        this.$navs   = {};
+        this.$settings = {  bar:{} , footer:{} , right:{actions:[]} , left:{}  };
+
+        this.init = function(element) { this.element = element; };
+
+        this.addBar = function(scope, element, attrs, id) {
+            $NavBarDelegate.addBar(scope, element, attrs, id);
+          };
+
+        this.addNav = function(scope, element, attrs, side) {
+            this.$navs[side] = new sides[side](scope, element, attrs);
+            this.$navs[side].activate();
+          };
+      }
+
+  }).call(this);
