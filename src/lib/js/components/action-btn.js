c634f12775c41a84bd024d823dc57b93ea9a1d8c
joelcoxokc <joelcox@hisimagination.com>
2015-02-07 09:40:10 -0800

    Update: Complete floating actions buttons

--- src/lib/js/components/action-btn.js
+++ src/lib/js/components/action-btn.js
@@ -2,7 +2,7 @@
     angular
         .module('mz.components.action-btn', [])
         .directive('mzActionBtn', mzActionBtn)
-        .directive('actionBtn', actionBtn)
+        // .directive('actionBtn', actionBtn)
         .directive('actionList', actionList)
         ;
 
@@ -12,44 +12,74 @@
                 , restrict   : 'E'
                 , replace    : true
                 , transclude : true
-                , scope      : {viz: '@', type:'@', size: '@'}
+                , controller : function(){}
+                , scope      : {viz: '@', type:'@', size: '@', icon:'@', nextIcon: '@',btnBg:'@'}
                 , link       : link
                 };
         function link(scope, element, attrs, ctrl, transclude) {
             scope.viz &&( element.addClass('action-btn-'+scope.viz) )
-            scope.size &&( element.addClass('mz-action-btn-'+scope.size) )
-          }
-      }
+            ctrl.viz = scope.viz
+            var btn = angular.element(element.children('a'));
 
-    /* @inject */
-    function actionBtn() {
-        return  { template   : '<a class="btn action-btn mz-btn"><i data-ng-if="icon" data-ng-class="icon"></i></a>'
-                , restrict   : 'E'
-                , replace    : true
-                , transclude : true
-                , scope      : {icon:'@', type:'@', size: '@'}
-                , link       : link
-                };
-        function link(scope, element, attrs, ctrl, transclude) {
-            scope.type &&( element.addClass('btn-'+scope.type) )
-            scope.size &&( element.addClass('btn-'+scope.size) )
+            btn.addClass('btn-'+scope.size)
+
+
+            scope.oldIcon = attrs.icon;
 
             transclude(scope, function (clone) {element.append(clone); });
-
           }
       }
+
+    // /* @inject */
+    // function actionBtn() {
+    //     return  { template   : ''
+    //             , restrict   : 'E'
+    //             , replace    : true
+    //             , transclude : true
+    //             , scope      : {icon:'@', nextIcon: '@', type:'@', size: '@'}
+    //             , link       : link
+    //             };
+    //     function link(scope, element, attrs, ctrl, transclude) {
+    //         scope.type &&( element.addClass('btn-'+scope.type) )
+    //         scope.size &&( element.addClass('btn-'+scope.size) )
+    //         scope.oldIcon = attrs.icon;
+
+    //         transclude(scope, function (clone) {element.append(clone); });
+
+    //       }
+    //   }
 
     /* @inject */
     function actionList() {
         return  { templateUrl   : 'components/action-list.html'
                 , restrict   : 'E'
+                , require    : '^mzActionBtn'
                 , replace    : true
                 , transclude : true
                 , scope      : {icon:'@', type:'@', size: '@'}
                 , link       : link
                 };
         function link(scope, element, attrs, ctrl, transclude) {
+            var child = element.children()
+            child = angular.element(child);
+            var length = child.children().length
 
+            angular.forEach(child.children(), function (item, index){
+                item = angular.element(item);
+
+                if(ctrl.viz === 'top') {
+                    item.addClass('btn-position-'+(length - index));
+                }
+                if(ctrl.viz === 'left') {
+                    item.addClass('btn-position-'+(length - index));
+                }
+                if(ctrl.viz === 'bottom') {
+                    item.addClass('btn-position-'+ (index+1));
+                }
+                if(ctrl.viz === 'right') {
+                    item.addClass('btn-position-'+ (index+1));
+                }
+            })
 
           }
       }
