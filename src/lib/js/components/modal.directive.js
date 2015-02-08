89a957d839bc8eba7860444f814e88ce1fb4b6db
joelcoxokc <joelcox@hisimagination.com>
2015-02-08 15:42:51 -0800

    Update: bower.json

--- src/lib/js/components/modal.directive.js
+++ src/lib/js/components/modal.directive.js
@@ -2,15 +2,41 @@
     angular
         .module('mz.components.modal', [])
         .directive('mzModal', mzModal)
+        .directive('mzToggleModal', mzToggleModal)
         ;
 
     /* @inject */
-    function mzModal() {
-        return  { restrict   : 'E'
-                , scope      : true
-                , transclude : true
-                , link       : function link(scope, element, attrs) {}
-                }; // templateUrl: 'templates/mzModal.view.html',
+    function mzModal($ModalService) {
+        return  { templateUrl : 'components/modal.html'
+                , restrict    : 'E'
+                , scope       : true
+                , replace     : true
+                , transclude  : true
+                , link        : link
+                };
+
+        function link(scope, element, attrs, ctrl) {
+            if (!attrs.id) return console.error('mzModalDirective requires a unique Id attribute');
+              /*==================================*/}
+
+      }
+
+    /* @inject */
+    function mzToggleModal($ModalService) {
+        return  { restrict   : 'A'
+                , link       : link
+                };
+        function link(scope, element, attrs) {
+            var toggle = attrs.toggle || attrs.mzToggleModal;
+            if (!toggle) return console.error('mzToggleModal requires a toggle attribute in order to toggle the correct modal');
+
+            $(document).ready(function(){
+                element.on('click', function(){
+                    $(toggle).openModal();
+                  });
+              });
+
+            /*==============================*/}
       }
 
   }).call(this);
--- src/lib/js/components/services/modal.service.js
+++ src/lib/js/components/services/modal.service.js
@@ -1,19 +1,58 @@
 ;(function() { 'use strict';
     angular
         .module('mz.components.services.modal', [])
-        .provider('mzModalService', mzModalService)
+        .factory('$ModalService', ModalService)
         ;
 
     /* @ngAnotate */
-    function mzModalService() {
-        // var _this = this;
-        this.init = function(element) { this.element = element; };
+    function ModalService() {
+        // var _this = _this;
+        var _this      = {};
+        _this.$toggles = {};
+        _this.$modals  = {};
 
-        this.$get = function($injector) {
-            function Modal() { /* var _this = this; */ }
-            Modal.prototype.enable = function() {};
-            return $injector.instantiate(Modal);
-          }
+        _this.register = function(element, id) {
+
+            _this.$modals[id]        = _this.$modals[id] || {};
+            _this.$modals[id].el     = $(element);
+            _this.$modals[id].isOpen = false;
+            /*=====================*/};
+
+        _this.registerToggle = function(element, id) {
+            _this.$modals[id] = _this.$modals[id] || {};
+
+            _this.$modals[id].toggle = $(element);
+
+            _this.$modals[id].toggle
+              .on('click', function(event){
+                  event.preventDefault();
+                  _this.toggle(id);
+                });
+                /*=====================*/};
+
+        _this.toggle = function(id) {
+
+            _this.$modals[id].isOpen ? _this.close(id) : _this.open(id);
+            /*=====================*/};
+
+        _this.open = function(id) {
+            console.log(_this.$modals[id].el)
+            _this.$modals[id].el.openModal();
+            _this.$modals[id].el.isOpen = true;
+            /*=====================*/};
+
+        _this.close = function(id) {
+
+            _this.$modals[id].el.closeModal();
+            _this.$modals[id].el.isOpen = false;
+            /*=====================*/};
+
+         return _this;
+
+
+        // _this.$get = function($injector) {
+        //     return $injector.instantiate(Modal);
+        //   }
       }
 
   }).call(this);
